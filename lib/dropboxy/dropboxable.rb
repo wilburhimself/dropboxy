efa9c120cb3f30876f6f3416edf13a96071e9d5a
Wilbur Suero <wilbur.himself@gmail.com>
2013-12-17 19:34:22 -0400

    Removed fetch_file from dropboxable

--- lib/dropboxy/dropboxable.rb
+++ lib/dropboxy/dropboxable.rb
@@ -1,16 +1,11 @@
 require 'RedCloth'
 require 'dropbox_sdk'
 module Dropboxable
-  
+
   class Client
 
     def initialize(access_token)
       @client = DropboxClient.new(access_token)
-    end
-
-    def fetch_file(file_path)
-      contents, metadata = @client.get_file_and_metadata(file_path)
-      contents
     end
 
     def method_missing(m, *args, &block)
