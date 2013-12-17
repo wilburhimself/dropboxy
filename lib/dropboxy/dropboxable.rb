2059c793f8eed83887b6005e072df3683d9f8dfb
josue.abreu@gmail.com <josue.abreu@gmail.com>
2013-12-17 15:11:43 -0400

    Client should have a getter/setter
    Also use the get_file function of the SDK if you only need the file and
    not the metadata. Added the documentation for more clarity.

--- lib/dropboxy/dropboxable.rb
+++ lib/dropboxy/dropboxable.rb
@@ -1,20 +1,31 @@
 require 'RedCloth'
 require 'dropbox_sdk'
 module Dropboxable
-  
+
   class Client
 
+    attr_reader :client
+
     def initialize(access_token)
-      @client = DropboxClient.new(access_token)
+      client = DropboxClient.new(access_token)
     end
 
+    # Wrapper function for the dropbox SDK get_file_and_metadata
+    # Download a file and get its metadata.
+    #
+    # Args:
+    # * +from_path+: The path to the file to be downloaded
+    # * +rev+: A previous revision value of the file to be downloaded
+    #
+    # Returns:
+    # * The file contents.
+    # * The file metadata as a hash.
     def fetch_file(file_path)
-      contents, metadata = @client.get_file_and_metadata(file_path)
-      contents
+      client.get_file(file_path)
     end
 
     def method_missing(m, *args, &block)
-      @client.send(m, *args)
+      client.send(m, *args)
     end
 
   end
